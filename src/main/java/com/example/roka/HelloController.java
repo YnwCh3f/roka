90063599c10b541766d583a9472e9c2437829d4f
YnwCh3f <benacsekd07@gmail.com>
2024-02-20 15:22:55 +0100

    Reflektor

--- src/main/java/com/example/roka/HelloController.java
+++ src/main/java/com/example/roka/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
     private ImageView[][] ivTomb = new ImageView[16][32];
     private String[] tomb = {"dark", "dead", "fox", "home", "target", "tree"};
     private int[][]t = new int[16][32];
-    private Image[] icon = new Image[5];
+    private Image[] icon = new Image[6];
 
     private final int DARK = 0;
     private final int DEAD = 1;
@@ -27,23 +27,43 @@ public class HelloController {
 
 
     public void initialize(){
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 6; i++){
             icon[i] = getKep(tomb[i]);
         }
+        generalErdo();
         for (int i = 0; i < 16; i++){ for (int j = 0; j < 32; j++) {
                 ivTomb[i][j] = new ImageView(icon[DARK]);
                 ivTomb[i][j].setTranslateX(48*j+10);
                 ivTomb[i][j].setTranslateY(48*i+10);
                 int ii = i; int jj = j;
-                ivTomb[i][j].setOnMouseEntered(e -> setAround(ii, jj, icon[HOME]));
+                ivTomb[i][j].setOnMouseEntered(e -> setAround(ii, jj));
                 ivTomb[i][j].setOnMouseExited(e -> setAround(ii, jj, icon[DARK]));
                 pn.getChildren().add(ivTomb[i][j]);
             }
         }
+
     }
 
     private Image getKep(String s){
         return new Image(getClass().getResourceAsStream(s+".png"));
+    }
+
+    private void setAround(int i, int j){
+        for (int ii = i-2; ii <= i+2; ii++){
+            for (int jj = j-1; jj <= j+1; jj++){
+                try{
+                    ivTomb[ii][jj].setImage(icon[t[ii][jj]]);
+                }catch (Exception e) {continue;}
+            }
+        }
+
+        for (int ii = i-1; ii <= i+1; ii++){
+            for (int jj = j-2; jj <= j+2; jj++){
+                try{
+                    ivTomb[ii][jj].setImage(icon[t[ii][jj]]);
+                }catch (Exception e) {continue;}
+            }
+        }
     }
 
     private void setAround(int i, int j, Image im){
@@ -64,12 +84,17 @@ public class HelloController {
         }
     }
 
+    private int roka = 0;
+    private int rokaMax = 0;
+
     private void generalErdo(){
         for (int i = 0; i < 16; i++){ for (int j = 0; j < 32; j++) {
-                if (Math.random() < 0.1)t[i][j] = FOX;
+                if (Math.random() < 0.1){t[i][j] = FOX; roka++;}
                 else t[i][j] = TREE;
             }
         }
+        rokaMax = roka;
+        lbRoka.setText(roka + " / " + rokaMax + " róka");
     }
 
 }
